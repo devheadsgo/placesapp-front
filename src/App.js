150adb5449846c82a7219ccb3198dfda89d9e85c
Enrique Juan de Dios <juandedioz@gmail.com>
2019-05-01 17:05:15 -0300

    [Setup] Added Login with Facebook Feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
+import { provider, auth } from "./config/firebase";
 
-const App = () => (
-  <div className="App">
-    <h1>Las Previas</h1>
-    <Button variant="raised" color="primary">
-      Comienza las previas
-    </Button>
-  </div>
-);
+const App = () => {
+  const [user, setUser] = useState(null);
+  const login = () => {
+    auth()
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        setUser(user);
+      });
+  };
+
+  const logout = () => {
+    auth()
+      .signOut()
+      .then(() => {
+        setUser(null);
+      });
+  };
+  return (
+    <div className="App">
+      <h1>Las Previas</h1>
+      {user ? (
+        <>
+          <p>Hey, {user.displayName}</p>
+          <Button variant="contained" onClick={logout}>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <Button variant="contained" onClick={login}>
+          Login with Facebook
+        </Button>
+      )}
+      <Button variant="contained" color="primary">
+        Comienza las previas
+      </Button>
+    </div>
+  );
+};
 
 export default App;
